39a17df8e2cc57f0c00e8abc465d9b7b10549649
Wouter Scherphof <wouter.scherphof@gmail.com>
2017-06-05 00:02:28 +0200

    examples in comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,15 +7,13 @@ x=$(($freeMem/10*8))
 n=$(($freeMem/10*2))
 export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 
-MODE="$1"
-
-case "${MODE}" in
+case "$1" in
 
 master)
 	shift 1
 	R="$*"
 	if [ "${R}" ]; then
-		R="-R ${R//\ /,}" # replace spaces for commas
+		R="-R ${R//\ /,}" # replace spaces with commas
 	fi
 
 	cd /results
@@ -28,6 +26,15 @@ master)
 		-t /jmx \
 		-l results.jtl -e -o dashboard \
 		${R}
+
+	# run example:
+	# docker run --rm --name master \
+	# 	--env IP=192.168.99.100 \
+	# 	--volume /home/docker/test_plan.jmx:/jmx \
+	# 	--volume /home/docker/results:/results \
+	# 	--publish 60000:60000 \
+	# 	wscherphof/jmeter:3.2 \
+	# 	master 192.168.99.103 192.168.99.104 192.168.99.105
 ;;
 
 slave)
@@ -37,7 +44,13 @@ slave)
 	    -D "java.rmi.server.hostname=${IP}" \
 	    -D "client.rmi.localport=${CLIENT_PORT}" \
 	    -D "server.rmi.localport=1099" \
-		-JJVM_ID=${IP//.}
+		-JJVM_ID=${IP//.} # remove dots
+
+	# run example:
+	# docker run --rm --detach --name slave 
+	# 	--env IP=192.168.99.103 \
+	# 	--publish 1099:1099 \
+	# 	wscherphof/jmeter:3.2
 ;;
 
 esac
